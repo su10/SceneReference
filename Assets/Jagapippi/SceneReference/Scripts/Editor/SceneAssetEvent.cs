06e6e862dda80d0c4dd8a73ad211fe10554048ed
su10 <1687366+su10@users.noreply.github.com>
2019-11-30 16:30:39 +0900

    シーンファイル（.unity）の追加・移動・削除のイベントを提供するクラス追加

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneAssetEvent.cs
@@ -0,0 +1,32 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Text.RegularExpressions;
+using UnityEditor;
+
+namespace Jagapippi.SceneReference
+{
+    public class SceneAssetEvent : UnityEditor.AssetModificationProcessor
+    {
+        public static event Action<string> onWillCreate;
+        public static event Action<string, string> onWillMove;
+        public static event Action<string> onWillDelete;
+
+        static void OnWillCreateAsset(string path)
+        {
+            if (path.EndsWith(".unity.meta")) onWillCreate?.Invoke(Regex.Replace(path, @".meta$", ""));
+        }
+
+        static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            if (sourcePath.EndsWith(".unity")) onWillMove?.Invoke(sourcePath, destinationPath);
+            return AssetMoveResult.DidNotMove;
+        }
+
+        static AssetDeleteResult OnWillDeleteAsset(string assetPath, RemoveAssetOptions option)
+        {
+            if (assetPath.EndsWith(".unity")) onWillDelete?.Invoke(assetPath);
+            return AssetDeleteResult.DidNotDelete;
+        }
+    }
+}
+#endif
