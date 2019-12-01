aa0b33d915c4d3ca7990e40f57a477a7d93b9adf
su10 <1687366+su10@users.noreply.github.com>
2019-12-02 00:44:02 +0900

    ISceneReferenceインターフェース追加

---
+++ Assets/Jagapippi/SceneReference/Scripts/ISceneReference.cs
@@ -0,0 +1,10 @@
+﻿namespace Jagapippi.SceneReference
+{
+    public interface ISceneReference
+    {
+        string name { get; }
+        string path { get; }
+        bool enabled { get; }
+        int buildIndex { get; }
+    }
+}
