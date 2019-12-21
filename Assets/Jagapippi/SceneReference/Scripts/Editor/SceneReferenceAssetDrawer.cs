c6a70de7c3ab340d6df1a8a36a9ecf5fd6148ece
su10 <1687366+su10@users.noreply.github.com>
2019-12-21 20:13:55 +0900

    SceneReferenceAssetを[SerializeField]で持たせたときのInspector上の表示をシーンファイルで置き換え
    NOTE:
    シーンファイルに対応するSceneReferenceAssetが存在しない場合は新しく作成する

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetDrawer.cs
@@ -0,0 +1,36 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.SceneReference
+{
+    [CustomPropertyDrawer(typeof(SceneReferenceAsset))]
+    public class SceneReferenceAssetDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            var sceneReferenceAsset = (SceneReferenceAsset) property.objectReferenceValue;
+            Object obj = (sceneReferenceAsset) ? sceneReferenceAsset.FindSceneAsset() : null;
+
+            var sceneAsset = (SceneAsset) EditorGUI.ObjectField(position, label, obj, typeof(SceneAsset), false);
+            var scenePath = AssetDatabase.GetAssetPath(sceneAsset);
+
+            if (sceneAsset == null || string.IsNullOrEmpty(scenePath))
+            {
+                property.objectReferenceValue = null;
+            }
+            else if (sceneReferenceAsset == null || sceneReferenceAsset.path != scenePath)
+            {
+                property.objectReferenceValue = FindOrCreateSceneReferenceAsset(scenePath);
+            }
+        }
+
+        private static SceneReferenceAsset FindOrCreateSceneReferenceAsset(string scenePath)
+        {
+            var asset = SceneReferenceAsset.FindByScenePath(scenePath);
+            if (asset == null) asset = SceneReferenceAsset.CreateByScenePath(scenePath);
+            return asset;
+        }
+    }
+}
+#endif
