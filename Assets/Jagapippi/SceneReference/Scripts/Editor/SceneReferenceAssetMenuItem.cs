196b26027e71a355f38bd50f163a940a2d870832
su10 <1687366+su10@users.noreply.github.com>
2019-12-21 20:08:06 +0900

    シーンファイルを選択したときのコンテキストメニューからSceneReferenceAssetのアセットファイルを作成できるようにした

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetMenuItem.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using System.Linq;
+using UnityEditor;
+
+namespace Jagapippi.SceneReference
+{
+    public static class SceneReferenceAssetMenuItem
+    {
+        private const string MenuItemPath = "Assets/Create/SceneReference";
+
+        [MenuItem(MenuItemPath, true)]
+        static bool MenuItemValidateFunction()
+        {
+            return Selection.objects.Select(AssetDatabase.GetAssetPath).Any(path => path.EndsWith(".unity"));
+        }
+
+        [MenuItem(MenuItemPath, false, 201)]
+        static void MenuItem()
+        {
+            var scenePaths = Selection.objects.Select(AssetDatabase.GetAssetPath).Where(path => path.EndsWith(".unity"));
+
+            foreach (var scenePath in scenePaths)
+            {
+                var asset = SceneReferenceAsset.FindByScenePath(scenePath);
+
+                if (asset)
+                {
+                    Debug.LogWarning($"{nameof(SceneReferenceAsset)} already exists for '{asset.path}'.");
+                }
+                else
+                {
+                    asset = SceneReferenceAsset.CreateByScenePath(scenePath);
+                }
+
+                Selection.activeObject = asset;
+            }
+        }
+    }
+}
+#endif
