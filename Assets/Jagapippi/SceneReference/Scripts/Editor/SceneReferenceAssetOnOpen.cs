49e89c4ce9b95e952891f898a74186d4d965086e
su10 <1687366+su10@users.noreply.github.com>
2019-12-21 20:16:14 +0900

    SceneReferenceAssetのアセットファイルをダブルクリックしたときの挙動をシーンファイルの挙動と同じにした

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetOnOpen.cs
@@ -0,0 +1,28 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.SceneManagement;
+
+namespace Jagapippi.SceneReference
+{
+    public static class SceneReferenceAssetOnOpen
+    {
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            // TODO: 再生中の場合の対応
+            if (EditorApplication.isPlayingOrWillChangePlaymode) return true;
+
+            var sceneReferenceAsset = EditorUtility.InstanceIDToObject(instanceID) as SceneReferenceAsset;
+            if (sceneReferenceAsset == null) return false;
+
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+            {
+                EditorSceneManager.OpenScene(sceneReferenceAsset.path);
+            }
+
+            return true;
+        }
+    }
+}
+#endif
