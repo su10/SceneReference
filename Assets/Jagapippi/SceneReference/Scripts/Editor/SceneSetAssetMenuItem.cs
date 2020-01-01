afcc8dc1c715fc1d839294b5df3b2819e2cfb87b
su10 <1687366+su10@users.noreply.github.com>
2020-01-01 11:32:50 +0900

    シーンファイルを選択したときのコンテキストメニューからSceneSetAssetのアセットファイルを作成できるようにした

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneSetAssetMenuItem.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using System.IO;
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.SceneReference
+{
+    public static class SceneSetAssetMenuItem
+    {
+        private const string MenuItemPath = "Assets/Create/SceneSet";
+        private static readonly string DefaultAssetName = "SceneSet.asset";
+        private static readonly string DefaultAssetPath = $"Assets/{DefaultAssetName}";
+
+        [MenuItem(MenuItemPath, true)]
+        static bool MenuItemValidateFunction() => true;
+
+        [MenuItem(MenuItemPath, false, 201)]
+        static void MenuItem()
+        {
+            var instance = ScriptableObject.CreateInstance<SceneSetAsset>();
+            {
+                var scenePaths = new List<string>();
+
+                if (Selection.activeObject)
+                {
+                    foreach (var obj in Selection.objects)
+                    {
+                        switch (obj)
+                        {
+                            case SceneAsset sceneAsset:
+                                scenePaths.Add(AssetDatabase.GetAssetPath(sceneAsset));
+                                break;
+                            case ISceneReference sceneReference:
+                                scenePaths.Add(sceneReference.path);
+                                break;
+                        }
+                    }
+                }
+
+                instance.AddAll(scenePaths);
+            }
+
+            var assetPath = DefaultAssetPath;
+
+            if (Selection.activeObject)
+            {
+                var selectedAssetPath = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+                if (File.GetAttributes(selectedAssetPath).HasFlag(FileAttributes.Directory))
+                {
+                    assetPath = $"{selectedAssetPath}/{DefaultAssetName}";
+                }
+                else
+                {
+                    assetPath = $"{Path.GetDirectoryName(selectedAssetPath)}/{DefaultAssetName}";
+                }
+            }
+
+            ProjectWindowUtil.CreateAsset(instance, assetPath);
+        }
+    }
+}
+#endif
