e9bf0a9af4c1aaa0cf8746dba8c88897058b803c
su10 <1687366+su10@users.noreply.github.com>
2020-01-01 11:40:29 +0900

    SceneSetAssetのアセットファイルをダブルクリックしたときに登録されているシーンをHierarchyに展開するようにした

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneSetAssetOnOpen.cs
@@ -0,0 +1,38 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.SceneManagement;
+
+namespace Jagapippi.SceneReference
+{
+    public static class SceneSetAssetOnOpen
+    {
+        [OnOpenAsset]
+        public static bool OnOpenAsset(int instanceID, int line)
+        {
+            // TODO: 再生中の場合の対応
+            if (EditorApplication.isPlayingOrWillChangePlaymode) return true;
+
+            var sceneSetAsset = EditorUtility.InstanceIDToObject(instanceID) as SceneSetAsset;
+            if (sceneSetAsset == null) return false;
+
+            if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo() == false) return true;
+
+            for (var i = 0; i < sceneSetAsset.Count; i++)
+            {
+                var sceneReference = sceneSetAsset[i];
+                if (sceneReference == null) continue;
+
+                EditorSceneManager.OpenScene(
+                    sceneReference.path,
+                    (i == 0)
+                        ? OpenSceneMode.Single
+                        : OpenSceneMode.AdditiveWithoutLoading
+                );
+            }
+
+            return true;
+        }
+    }
+}
+#endif
