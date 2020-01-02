3c249eaadde501aee0113e09fd53ad3abf105b09
su10 <1687366+su10@users.noreply.github.com>
2020-01-03 00:52:16 +0900

    SceneSetAssetの編集でなるべく要素がNoneにならないようにした

--- Assets/Jagapippi/SceneReference/Scripts/Editor/SceneSetAssetEditor.cs
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneSetAssetEditor.cs
@@ -11,28 +11,26 @@ namespace Jagapippi.SceneReference
         private static readonly string PropertyName = "_sceneReferenceAssets";
         private SerializedProperty _property;
         private ReorderableList _reorderableList;
+        private int _lastElementControlID = 0;
 
         void OnEnable()
         {
             _property = this.serializedObject.FindProperty(PropertyName);
             _reorderableList = this.CreateReorderableList(_property);
+            _lastElementControlID = EditorGUIUtility.GetControlID(FocusType.Passive);
         }
 
         public override void OnInspectorGUI()
         {
             this.serializedObject.Update();
+            this.HandleObjectPicker(_lastElementControlID);
             _reorderableList.DoLayoutList();
             this.serializedObject.ApplyModifiedProperties();
         }
 
         void OnDisable()
         {
-            for (var i = 0; i < _property.arraySize; i++)
-            {
-                if (_property.GetArrayElementAtIndex(i).objectReferenceValue) continue;
-                _property.DeleteArrayElementAtIndex(i);
-                i--;
-            }
+            RemoveNull(_reorderableList);
 
             if (this.serializedObject.targetObject)
             {
@@ -41,6 +39,7 @@ namespace Jagapippi.SceneReference
 
             _property = null;
             _reorderableList = null;
+            _lastElementControlID = 0;
         }
 
         private ReorderableList CreateReorderableList(SerializedProperty property)
@@ -80,6 +79,12 @@ namespace Jagapippi.SceneReference
                     elementProperty.objectReferenceValue = SceneReferenceAsset.FindOrCreate(selectedSceneAsset);
                 }
             };
+            reorderableList.onAddCallback += (list) =>
+            {
+                list.serializedProperty.arraySize += 1;
+                list.serializedProperty.GetArrayElementAtIndex(list.count - 1).objectReferenceValue = null;
+                EditorGUIUtility.ShowObjectPicker<SceneAsset>(null, false, "", _lastElementControlID);
+            };
             reorderableList.onRemoveCallback += (list) =>
             {
                 var count = list.serializedProperty.arraySize;
@@ -92,6 +97,44 @@ namespace Jagapippi.SceneReference
             };
 
             return reorderableList;
+        }
+
+        private void HandleObjectPicker(int controlID)
+        {
+            switch (Event.current.commandName)
+            {
+                case "ObjectSelectorUpdated":
+                    if (EditorGUIUtility.GetObjectPickerControlID() != controlID) return;
+
+                    var selectedScene = (SceneAsset) EditorGUIUtility.GetObjectPickerObject();
+                    var elementProperty = _property.GetArrayElementAtIndex(_property.arraySize - 1);
+                    if (selectedScene)
+                    {
+                        elementProperty.objectReferenceValue = SceneReferenceAsset.FindOrCreate(selectedScene);
+                    }
+                    else
+                    {
+                        elementProperty.objectReferenceValue = null;
+                    }
+
+                    break;
+                case "ObjectSelectorClosed":
+                    RemoveNull(_reorderableList);
+                    break;
+            }
+        }
+
+        private static void RemoveNull(ReorderableList list)
+        {
+            for (var i = 0; i < list.count; i++)
+            {
+                var property = list.serializedProperty;
+                var elementProperty = property.GetArrayElementAtIndex(i);
+                if (elementProperty.objectReferenceValue) continue;
+
+                property.DeleteArrayElementAtIndex(i);
+                i--;
+            }
         }
     }
 }
