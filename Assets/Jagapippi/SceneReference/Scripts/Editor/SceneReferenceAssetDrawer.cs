ceb8fbd91fed0148a91cf118e16e2c65f2c72df6
su10 <1687366+su10@users.noreply.github.com>
2020-01-12 18:25:44 +0900

    SceneReferenceAssetDrawerのOdin対応

--- Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetDrawer.cs
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetDrawer.cs
@@ -1,4 +1,4 @@
-﻿#if UNITY_EDITOR
+﻿#if UNITY_EDITOR && !ODIN_INSPECTOR
 using UnityEditor;
 using UnityEngine;
 
---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetDrawerWithOdin.cs
@@ -0,0 +1,33 @@
+﻿#if UNITY_EDITOR && ODIN_INSPECTOR
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.SceneReference
+{
+    public class SceneReferenceAssetDrawerWithOdin : OdinValueDrawer<SceneReferenceAsset>
+    {
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            var rect = EditorGUILayout.GetControlRect();
+            if (label != null) rect = EditorGUI.PrefixLabel(rect, label);
+
+            var sceneReferenceAsset = this.ValueEntry.SmartValue;
+            Object obj = (sceneReferenceAsset) ? sceneReferenceAsset.FindSceneAsset() : null;
+
+            var sceneAsset = (SceneAsset) SirenixEditorFields.UnityObjectField(rect, obj, typeof(SceneAsset), false);
+            var scenePath = AssetDatabase.GetAssetPath(sceneAsset);
+
+            if (sceneAsset == null || string.IsNullOrEmpty(scenePath))
+            {
+                this.ValueEntry.SmartValue = null;
+            }
+            else if (sceneReferenceAsset == null || sceneReferenceAsset.path != scenePath)
+            {
+                this.ValueEntry.SmartValue = SceneReferenceAsset.FindOrCreate(scenePath);
+            }
+        }
+    }
+}
+#endif
