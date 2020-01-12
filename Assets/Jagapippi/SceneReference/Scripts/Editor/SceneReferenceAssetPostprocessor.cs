e1a34dd4821788d591ef8f08d0abd8d891ccb7c8
su10 <1687366+su10@users.noreply.github.com>
2020-01-12 21:36:01 +0900

    SceneReferenceAssetが複製されたときに削除する処理を別クラスに分割

---
+++ Assets/Jagapippi/SceneReference/Scripts/Editor/SceneReferenceAssetPostprocessor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace Jagapippi.SceneReference
+{
+    public class SceneReferenceAssetPostprocessor : AssetPostprocessor
+    {
+        static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+        {
+            List<SceneReferenceAsset> allAssets = null;
+
+            foreach (var path in importedAssets)
+            {
+                if (path.EndsWith(".asset") == false) continue;
+
+                var importedAsset = AssetDatabase.LoadAssetAtPath<SceneReferenceAsset>(path);
+                if (importedAsset == null) continue;
+
+                if (allAssets == null) allAssets = SceneReferenceAsset.FindAll().ToList();
+                if (allAssets.All(asset => asset == importedAsset || asset.path != importedAsset.path)) continue;
+
+                Debug.LogWarning($"{nameof(SceneReferenceAsset)} already exists for '{importedAsset.path}'.");
+                importedAsset.Delete();
+            }
+        }
+    }
+}
--- Assets/Jagapippi/SceneReference/Scripts/SceneReferenceAsset.cs
+++ Assets/Jagapippi/SceneReference/Scripts/SceneReferenceAsset.cs
@@ -185,31 +185,9 @@ namespace Jagapippi.SceneReference
             if (assetPath == this.path) this.Delete();
         }
 
-        private void Delete()
+        public void Delete()
         {
             if (this) AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(this));
-        }
-
-        private class AssetPostprocessor : UnityEditor.AssetPostprocessor
-        {
-            static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
-            {
-                List<SceneReferenceAsset> allAssets = null;
-
-                foreach (var path in importedAssets)
-                {
-                    if (path.EndsWith(".asset") == false) continue;
-
-                    var importedAsset = AssetDatabase.LoadAssetAtPath<SceneReferenceAsset>(path);
-                    if (importedAsset == null) continue;
-
-                    if (allAssets == null) allAssets = FindAll().ToList();
-                    if (allAssets.All(asset => asset == importedAsset || asset.path != importedAsset.path)) continue;
-
-                    Debug.LogWarning($"{nameof(SceneReferenceAsset)} already exists for '{importedAsset.path}'.");
-                    importedAsset.Delete();
-                }
-            }
         }
 #endif
     }
